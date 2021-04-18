f2fd9b4ea5554e91e9bea864569371d10f1eac08
이용구 <koo@iyong-guui-MacBookAir.local>
2021-04-18 19:08:38 +0900

    비밀 정호 보호

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const {User} = require("./models/User")
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://root:1234@koo.3tcyq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -9,7 +14,18 @@ mongoose.connect('mongodb+srv://root:1234@koo.3tcyq.mongodb.net/myFirstDatabase?
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World! HIHI!!')
+  res.send('KOO HI!!')
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
